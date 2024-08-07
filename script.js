0503224816f1beb16db199ce5711014fa326cd82
saaymary <sushchik.maria@gmail.com>
2024-08-07 15:46:39 +0300

    создали предсказания на js

--- script.js
+++ script.js
@@ -13,3 +13,42 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+
+
+const predictions = [
+    "Все будет супер!",
+    "Все будет еще лучше!",
+    "Затвра будет лучше, чем вчера!",
+    "Вскоре ты встретишь старого друга, и это будет замечательная встреча!",
+    "У тебя будет возможность реализовать свою мечту!",
+];
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const forecastBtn = document.querySelector('.forecast-btn');
+const currentForecastTitle = document.querySelector('.current-forecast h1');
+const currentForecastParagraph = document.querySelector('.current-forecast p');
+const forecastsContainer = document.querySelector('.forecasts');
+const forecastTemplate = document.getElementById('forecast-item').content;
+
+forecastBtn.addEventListener('click', () => {
+    const predictionIndex = getRandomNumber(0, predictions.length - 1);
+    const predictionText = predictions[predictionIndex];
+    const successProbability = getRandomNumber(0, 100);
+
+    currentForecastTitle.textContent = predictionText;
+    currentForecastParagraph.textContent = `Вероятность: ${successProbability}%`;
+
+    addForecastToList(predictionText, successProbability);
+});
+
+function addForecastToList(predictionText, successProbability) {
+    const forecastItem = document.importNode(forecastTemplate, true);
+    forecastItem.querySelector('h3').textContent = predictionText;
+    forecastItem.querySelector('p').textContent = `Вероятность: ${successProbability}%`;
+
+    forecastsContainer.prepend(forecastItem);
+}
